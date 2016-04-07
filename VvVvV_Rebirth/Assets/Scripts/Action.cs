8d6bec3d0aca7d099a847925c8f443320f3758f3
Lucas <lucas@localhost.localdomain>
2016-04-07 16:32:09 +0200

    Ajout des actions et de la list d'action

---
+++ VvVvV_Rebirth/Assets/Scripts/Action.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Unite
+{
+	public class Action
+	{
+		int cost_in_pa;
+
+
+		public Action (int id)
+		{
+			switch (id) {
+			case 0:
+				MoveTo (1);
+				break;
+			case 1:
+				PutTrap (1);
+				break;
+			case 2:
+				Wait ();
+				break;
+
+			}
+
+
+
+
+		}
+
+		public void MoveTo(int go_to)
+		{
+			cost_in_pa = 1;
+		}
+
+		public void Wait()
+		{
+			cost_in_pa = 3;
+		}
+
+		public void PutTrap(int pos)
+		{
+
+			cost_in_pa = 2;
+		}
+
+
+
+		void setCostPA(int _value)
+		{
+			cost_in_pa = _value;
+		}
+
+		public int getCostPa()
+		{
+			return cost_in_pa;
+		}
+
+		/*
+		public void setAction(Action _action_to_do)
+		{
+			action_to_do = _action_to_do;
+			Console.WriteLine ("Action ADD to task");
+		}
+
+		public Action getAction()
+		{
+			return action_to_do;
+		}*/
+
+	}
+}
+
---
+++ VvVvV_Rebirth/Assets/Scripts/Player.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Unite
+{
+	public class Player
+	{
+
+		List<Action> task_list;
+		Task actualTask;
+
+		int action_point;
+		int max_action_point;
+
+		public Player ()
+		{
+			max_action_point = 10;
+			action_point = max_action_point;
+
+
+			task_list = new List<Action> ();
+
+		}
+
+		public void InitNewTurn()
+		{
+
+			RemoveAllTaskToDo ();
+			action_point = max_action_point;
+
+			Console.WriteLine ("We are ready for a new turn");
+			Console.WriteLine ("action Point :" + action_point); 
+
+		}
+
+		public void ExecuteTurn ()
+		{
+			//Unity code a 100 %%%%%%
+
+
+		}
+
+		public void EndTurn()
+		{
+			Console.WriteLine ("TASK TO DO : " + task_list.Count );
+			Console.WriteLine ("action Point :" + action_point );
+		}
+
+
+		public void AddTaskToDo(int _idAction)
+		{
+			Action myAct = new Action (_idAction);
+			task_list.Add (myAct);
+			SoustractActionPoint (myAct.getCostPa ());
+			Console.WriteLine ("Nouvelle tâche ajouté à la liste !");
+		}
+
+		public void RemoveAllTaskToDo()
+		{
+
+			task_list.RemoveRange (0, task_list.Count );
+			action_point = max_action_point;
+			Console.WriteLine ("Liste des taches purgé!");
+		}
+
+		public void RemoveLastAddedTaskToDo()
+		{
+			AddActionPoint (task_list [task_list.Count - 1].getCostPa ());
+
+			task_list.RemoveAt(task_list.Count - 1);
+			Console.WriteLine ("Derniere tache supprimé!");
+		}
+
+		public void AddActionPoint(int _value)
+		{
+			action_point += _value;
+		}
+
+		public void SoustractActionPoint(int _value)
+		{
+			action_point -= _value;
+		}
+
+		public void SetMaxPointAction( int _value)
+		{
+			max_action_point = _value;
+		}
+
+		public int GetMaxPointAction()
+		{
+			return max_action_point;
+		}
+	}
+}
+
